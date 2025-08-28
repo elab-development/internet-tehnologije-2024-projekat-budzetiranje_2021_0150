75d9a17721b870455c356a3ed1c762ce03586923
anaspajic <anaspajic97@gmail.com>
2025-08-28 11:38:44 +0200

    Izmenjena logika za database seeder klasu

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\GroupExpense;
+use App\Models\DebtClaim;
+use App\Models\PersonalExpense;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(5)->state(['role' => 'admin'])->create();
+        $users = User::factory(20)->create();
+        $groups = Group::factory(5)
+            ->hasAttached($users->random(10)) 
+            ->create();
+    
+        
+        $expenses = GroupExpense::factory(10)->create();
+        $personalExpenses = PersonalExpense::factory(10)->create();
+    
+      
+        $expenses->each(function (GroupExpense $expense) {
+            $groupUsers = $expense->group->users;
+            $creditor = $expense->payer; 
+            $debtAmount = $expense->amount / ($groupUsers->count() - 1); 
+     
+            $groupUsers->where('id', '!=', $creditor->id)->each(function ($debtor) use ($expense, $creditor, $debtAmount) {
+                DebtClaim::create([
+                    'expense_id' => $expense->id,
+                    'creditor_id' => $creditor->id,
+                    'debtor_id' => $debtor->id,
+                    'amount' => $debtAmount,
+                    'status' => 'unpaid'
+                ]);
+            });
+        });
     }
+    
 }
