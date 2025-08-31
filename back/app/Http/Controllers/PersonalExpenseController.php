0f75cd38c0c3488a1c94b4f6f359ffdca1b4f583
marijaasim <marijaasim@gmail.com>
2025-08-31 10:13:09 +0200

    Kreirana metoda store u PersonalExpenseController-u koja kreira licni trosak

---
+++ back/app/Http/Controllers/PersonalExpenseController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\PersonalExpense;
+use App\Http\Resources\PersonalExpenseResource;
+use Illuminate\Http\Request;
+
+class PersonalExpenseController extends Controller
+{
+    public function store(Request $request)
+    {
+        try {
+
+            if(Auth::user()->role!='vip'){
+                return response()->json([
+                    'success' => false,
+                  'message' => 'Unauthorized user. You must have a VIP role to add a personal expense!!!',
+                ], 401);
+            }
+            $validated = $request->validate([
+                'name' => 'required|string|max:255|unique:personal_expenses,name',
+                'amount' => 'required|numeric|min:0.01',
+            ]);
+            $personalExpense = PersonalExpense::create([
+                'name' => $validated['name'],
+                'date' => now(),
+                'amount'=>$validated['amount'] ,
+                'user_id'=>Auth::user()->id
+            ]);
+
+          
+            return response()->json([
+                'message' => 'The personal expense has been successfully added.',
+                'data' => $personalExpense,
+            ], 201); 
+        } catch (\Exception $e) {
+         
+            return response()->json([
+                'error' => 'Failed to add the personal expense. Please try again.',
+            ], 500); 
+        }
+    }
+
+
+
+    
+
+
+
+
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PersonalExpenseController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,7 +15,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('users',[UserController::class,'index']);
     Route::get('users/edit',[UserController::class,'allWithoutAdmin']);
     Route::put('users/change-role',[UserController::class,'changeUserRole']);
-     Route::delete('users/{id}',[UserController::class,'destroy']);
+    Route::delete('users/{id}',[UserController::class,'destroy']);
+
+
+
+    Route::post('vip/personal-expenses',[PersonalExpenseController::class,'store']);
  
    
     
